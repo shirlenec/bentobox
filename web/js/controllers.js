08f10352d5e6f167754d8550d2c137cb0f564cda
Shirlene Chan <s.steph.chan@gmail.com>
2014-09-20 14:40:23 -0400

    added basic functionality for front page

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -8,6 +8,36 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
   function($scope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
+    $scope.ingredientToAdd = "";
+    $scope.newIngredients = [];
+    $scope.addIngredient = function () {
+      angular.element("#ingredient-list")[0].innerHTML += "<br>" + $scope.ingredientToAdd;
+      $scope.newIngredients.push($scope.ingredientToAdd);
+      $scope.ingredientToAdd = "";
+    }
+
+    $scope.essentials = [ 'eggs', 'flour', 'sugar', 'butter', 'oil', 'water', 'milk' ];
+
+    $scope.selection = $scope.essentials.slice(0);
+
+    $scope.toggleSelection = function(essential) {
+      var idx = $scope.selection.indexOf(essential);
+
+      // is currently selected
+      if (idx > -1) {
+        $scope.selection.splice(idx, 1);
+      }
+
+      // is newly selected
+      else {
+        $scope.selection.push(fruitName);
+      }
+    }
+
+    $scope.findRecipes = function() {
+      $scope.allIngredients = $scope.newIngredients.concat($scope.selection);
+      console.log($scope.allIngredients);
+    }
   }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
@@ -20,3 +50,4 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Ph
       $scope.mainImageUrl = imageUrl;
     }
   }]);
+
