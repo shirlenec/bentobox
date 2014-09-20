36bd90a4164b382d703d8b9151842f21d60690f8
Michelle <michellewkong@gmail.com>
2014-09-20 19:20:58 -0400

    changes

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,6 +1,6 @@
 var app = angular.module('recipeFinder', []);
 
-app.controller('getController', function($scope, recipeService){
+app.controller('getController', function($scope){
   function init() {
    // var data = recipeService.getRecipes(callback(data));  
    $.ajax({
@@ -24,5 +24,71 @@ app.controller('getController', function($scope, recipeService){
     
   }
 
-  init();
+  function getRecipes (ingredientList, essentialList) {
+    var ingredient;
+    var results = [];
+    // Check API for each ingredient individually. Add all to a list of all results, which may include duplicates or uncorrect items. 
+    $.when(
+    for (i in ingredientList){
+      var queryString = "https://api.yummly.com/v1/api/recipes?_app_id=0375a96b&_app_key=ad073d0bd45d862d60e9f41b30ad316a&q=" + ingredientList[i];
+
+      $.ajax({
+        method:'GET',
+        url:queryString,
+        dataType: 'jsonp',
+        headers:{
+                  'Access-Control-Allow-Origin': '*',
+                  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                  'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With'
+                }
+      }).
+      success (function(data, status, headers, config){
+        results = results.concat(data.matches);
+        console.log("results "  + results);
+      }).
+      error(function(data, status){
+        alert ('not working');
+      })
+    })
+    .done(filterRecipes(ingredientList, essentialList, results));
+}
+
+function filterRecipes (ingredientList, essentialList, results){
+    // Sort results
+    console.log(ingredientList);
+    console.log("essentialList" + essentialList);
+    console.log("results" + results);
+    results.sort(function(a, b) {
+      return a.id < b.id;
+    });
+ console.log("sorted results "  + results);
+    // Iterate through list, remove duplicates and recipes with ingredients that are provided. 
+    var filteredRecipes = [];
+    var allIngredients = ingredientList.concat(essentialList);
+    for (i in results){
+      if (i != 0 && results[i] != results[i-1]) {
+        // Check if recipe contains ingredients we don't have. 
+        var currentIngredients = results[i].ingredients;
+        var inList = true;
+          for (k in currentIngredients){
+            console.log("an ingredient: " + currentIngredients[k])
+            if (!jQuery.inArray(currentIngredients[k], allIngredients)) {
+              console.log("not in array: "+ currentIngredients[k]);
+              inList = false;
+              break;
+            }
+          }
+
+          if (inList){
+            console.log("added");
+            filteredRecipes.push(results[i]);
+          }
+      }
+    }
+console.log(filteredRecipes);
+    return filteredRecipes;
+  }
+
+  getRecipes(["boneless chicken skinless thigh","dry white wine","chicken stock","heavy cream","grated lemon zest","pappardelle", "salt", "pasta", "flour", 
+    "eggs", "black pepper", "ground black pepper", "yeast", "sugar", "water", "beef"], []);
 });
